e0eaf585fd55f58ffbd8b772da04c8e9244d362c
Ben Lieberman <bhlieberman93@gmail.com>
2024-08-29 14:29:55 -0600

    remove dead code and add readme

--- lib/core.ml
+++ lib/core.ml
@@ -1,41 +1,13 @@
 open Nottui
-open Notty
 
 module P = Nottui_pretty
 
 module W = Nottui_widgets
 
-let canto_i = In_channel.with_open_text "/home/slothrop/dev/ocaml/niaserie/assets/nia/canto_I.txt"
-  In_channel.input_all
-
-let canto_ii = In_channel.with_open_text "/home/slothrop/dev/ocaml/niaserie/assets/nia/canto_II.txt"
-  In_channel.input_all
-
-let canto_iv = In_channel.with_open_text "/home/slothrop/dev/ocaml/niaserie/assets/nia/canto_IV.txt"
-  In_channel.input_all
 
 let base = Lwd.var W.empty_lwd
 
 let wm = W.window_manager (Lwd.join (Lwd.get base))
-
-let canto_selector text f choices =
-  let curr = Lwd.var (match text with | "Canto I" -> canto_i
-  | "Canto II" -> canto_ii | "Canto IV" -> canto_iv | _ -> failwith "No match") in
-  W.vbox [
-    W.main_menu_item wm text (fun () ->
-    Lwd.pure @@ Ui.vcat (
-      List.map (fun choice -> W.sub_entry choice (fun () -> f choice)) choices
-    ));
-    (Lwd.map (Lwd.get curr) ~f:(fun s -> W.string ~attr:A.(bg red ++ st bold) s))
-    ]
-
-let cantos = 
-  let cs = ["Canto I"; "Canto II"; "Canto IV"] in
-  let choice = Lwd.var (List.hd cs) in
-  Lwd.join (
-    Lwd.map (Lwd.get choice)
-      ~f:(fun current -> canto_selector current (Lwd.set choice) cs)
-  )
 
 let doc: ((P.t Lwd.t) Lwd_table.t) = Lwd_table.make ()
 
